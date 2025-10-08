db138bf17073d20c2069d4d0166e08f1bc9fd231
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-08 00:51:55 -0300

    refactor: remove unused type

--- src/formats/html-format.ts
+++ src/formats/html-format.ts
@@ -1,11 +1,6 @@
 import type { SpecialValueProp } from "@/extractors";
 import type { HtmlTagEnum } from "../enums/html-type.enum";
 
-export type HtmlList = {
-	type: HtmlTagEnum.LI;
-	content: string;
-};
-
 export type HtmlFormat = {
 	type: HtmlTagEnum;
 	content?: string;

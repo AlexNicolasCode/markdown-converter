b0003d9bc52eb3efd0faf8c7b56063a0da185e21
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-07 21:28:00 -0300

    chore: update test shortcut

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
+  preset: 'ts-jest',
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+      "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
+  ]
 };
